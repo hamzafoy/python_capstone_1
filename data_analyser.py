6a9943e2ba321db755ec60f035c6fe2742afb8cf
hamzafoy <hamzaoflouisville@gmail.com>
2022-11-18 07:38:51 -0500

    finished out the data analysis function

--- data_analyser.py
+++ data_analyser.py
@@ -10,10 +10,40 @@ def find_min_among_sieges(data):
     for row in data:
         year_list.append(row[2])
     return year_list
-    # pprint(year_list)
+
+def search_city_among_sieges(data):
+    city_list = []
+    search_item = input("Enter what city you want to search the data for: ")
+    for row in data:
+        if row[3] == search_item:
+            city_list.append(row[3])
+    return city_list, search_item
+
+def search_country_among_sieges(data):
+    country_list = []
+    search_item = input("Enter what country you want to search the data for: ")
+    for row in data:
+        if row[4] == search_item:
+            country_list.append(row[4])
+    return country_list, search_item
+
+def search_sieges_for_city(data):
+    search_for_city = search_city_among_sieges(data)
+    result = 'is not found'
+    if len(search_for_city[0]) >= 1:
+        result = 'is found'
+    return "The city of " + search_for_city[1] + " " + result + " in the dataset."
+
+def search_sieges_for_country(data):
+    search_for_country = search_country_among_sieges(data)
+    result = 'is not found'
+    if len(search_for_country[0]) >= 1:
+        result = 'is found'
+    return "The country of " + search_for_country[1] + " " + result + " in the dataset."
 
 def count_sieges(data):
     year_list = find_min_among_sieges(data)
     return "There has been " + str(len(data)) + " sieges from year " + str(min(year_list)) + " to " + str(max(year_list)) + "."
 
 pprint(count_sieges(dataset))
+pprint(search_sieges_for_country(dataset))
--- data_wrangler.py
+++ data_wrangler.py
@@ -13,18 +13,9 @@ def generate_siege_data(path_string):
     with open(path_string) as siege_data:
         csv_data = csv.reader(siege_data, delimiter=',')
         for row in csv_data:
-            # row[1].replace(u"\uFFFD", "?").encode("utf-8")
-            # row[3].replace(u"\uFFFD", "?").encode("utf-8")
             for item in row:
                 if isinstance(item, str):
                     row[row.index(item)] = clean_space(item)
             list_of_sieges.append(row)
         del list_of_sieges[0]
     return list_of_sieges
-
-
-
-
-
-#data = pd.read_csv('./data/siege_locations.csv')
-#print(data)
