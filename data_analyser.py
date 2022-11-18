bb9147660a4cb4cfdb79bf704065dba589275a74
hamzafoy <hamzaoflouisville@gmail.com>
2022-11-18 07:20:14 -0500

    adding additional functions to analyze data.

--- data_analyser.py
+++ data_analyser.py
@@ -5,7 +5,15 @@ path_to_data = "./data/siege_locations.csv" # Path to the siege_locations.csv fi
 dataset = generate_siege_data(path_to_data)
 #pprint(generate_siege_data(path_to_data))
 
+def find_min_among_sieges(data):
+    year_list = []
+    for row in data:
+        year_list.append(row[2])
+    return year_list
+    # pprint(year_list)
+
 def count_sieges(data):
-    return "There has been " + str(len(data)) + " sieges from year 1600 to 1800"
+    year_list = find_min_among_sieges(data)
+    return "There has been " + str(len(data)) + " sieges from year " + str(min(year_list)) + " to " + str(max(year_list)) + "."
 
 pprint(count_sieges(dataset))
